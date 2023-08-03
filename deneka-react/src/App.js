8fe0b36e59a7a37a16f6cde03d5d5ab93ca63686
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-08-03 19:18:43 +1000

    Created a demo dashboard

--- deneka-react/src/App.js
+++ deneka-react/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/dashboard">Company</a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/dashboard">Dashboard</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/reports">Reports</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/settings">Settings</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/logout">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <header>
+        <h1 className="mt-4">Welcome to Company Dashboard</h1>
       </header>
+      <main>
+        <p className="lead">This is your starting point to navigating the company dashboard.</p>
+      </main>
     </div>
   );
 }
