54705a0e1778b86f4527a97fd9b8c64eae0036c4
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-08-23 19:47:30 +1000

    Improved UI

--- deneka-react/src/pages/Appheader/Appheader.jsx
+++ deneka-react/src/pages/Appheader/Appheader.jsx
@@ -4,6 +4,9 @@ import { CgWebsite } from "react-icons/cg";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
 import { removeToken } from "../../helpers";
+import './AppHeader.css'; // Importing CSS for styling
+import logo from './Deneka-One.png'; // Import the logo
+
 
 const AppHeader = () => {
   const { user } = useAuthContext();
@@ -17,7 +20,7 @@ const AppHeader = () => {
   return (
     <Space className="header_space">
       <Button className="header_space_brand" href="/" type="link">
-        <CgWebsite size={64} />
+        <img src={logo} alt="Deneka-One Logo" className="logo" /> {/* Included the logo */}
       </Button>
       <Space className="auth_buttons">
         {user ? (
@@ -25,11 +28,7 @@ const AppHeader = () => {
             <Button className="auth_button_login" href="/profile" type="link">
               {user.username}
             </Button>
-            <Button
-              className="auth_button_signUp"
-              type="primary"
-              onClick={handleLogout}
-            >
+            <Button className="auth_button_signUp" type="primary" onClick={handleLogout}>
               Logout
             </Button>
           </>
@@ -38,12 +37,8 @@ const AppHeader = () => {
             <Button className="auth_button_login" href="/signin" type="link">
               Login
             </Button>
-            <Button
-              className="auth_button_signUp"
-              href="/signup"
-              type="primary"
-            >
-              SignUp
+            <Button className="auth_button_signUp" href="/signup" type="primary">
+              Sign Up
             </Button>
           </>
         )}
