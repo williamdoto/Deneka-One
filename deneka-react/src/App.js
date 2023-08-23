c30a21cd26b13a99fa2c3d875ce411869d59b1a5
Hiu Lam Lau <hlau0019@student.monash.edu>
2023-08-23 18:50:46 +1000

    error solved in App.js

--- deneka-react/src/App.js
+++ deneka-react/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
