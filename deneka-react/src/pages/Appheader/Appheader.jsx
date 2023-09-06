f000f2bb88fbfbba1cf7c6c1b1958d6a9cf5ccbc
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-09-06 17:51:37 +1000

    Pushed the Navbar Demo

--- deneka-react/src/pages/Appheader/Appheader.jsx
+++ deneka-react/src/pages/Appheader/Appheader.jsx
@@ -43,11 +43,7 @@ const AppHeader = () => {
             <a href="#"><span class="fa fa-paper-plane"></span> Contacts</a>
           </li>
         </ul>
-        <div class="footer">
-        	<p>
-					  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-					</p>
-        </div>
+
         {/* ...same as before... */}
       </nav>
     </div>
